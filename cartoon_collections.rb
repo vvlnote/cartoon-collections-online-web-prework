ee2806786f259b65eb5a073b82dd586c9b923795
Joy Lin <vvlnote@hotmail.com>
2019-02-15 21:00:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  array.each_with_index { |val, index|
+  puts "#{index+1} #{val}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect {|item|
+    item.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.find {|item|
+    item.length > 4
+  }
 end
 
 def find_the_cheese# code an argument here
